e015f28c61c2b829b96cb338192f9b04249f851a
Ola Ali <ola.ali@ibtikar.net.sa>
2016-12-13 15:17:31 +0300

    Timeline Screen

--- Service/NotFoundExceptionListener.php
+++ Service/NotFoundExceptionListener.php
@@ -6,28 +6,32 @@ use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
-
-class NotFoundExceptionListener {
+class NotFoundExceptionListener
+{
 
     private $templating;
-    public function __construct($templating) {
+
+    public function __construct($templating)
+    {
         $this->templating = $templating;
     }
+
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
 
         if ($event->getException() instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
 
-            if(strpos($event->getRequest()->getRequestUri(),'backend')){
+            if (strpos($event->getRequest()->getRequestUri(), 'backend')) {
                 $response = $this->templating->renderResponse('IbtikarGlanceDashboardBundle:Exception:error.html.twig');
+                $event->setResponse($response);
+            } else {
+                $response = $this->templating->renderResponse('IbtikarGoodyFrontendBundle:Exception:error.html.twig');
                 $event->setResponse($response);
             }
         }
         if ($event->getException() instanceof \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException) {
-                $response = $this->templating->renderResponse('IbtikarGlanceDashboardBundle:Exception:denied.html.twig');
-                $event->setResponse($response);
-
+            $response = $this->templating->renderResponse('IbtikarGlanceDashboardBundle:Exception:denied.html.twig');
+            $event->setResponse($response);
         }
     }
-
 }
