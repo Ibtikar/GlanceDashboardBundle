096fc1ddd85120375b6f482c8a3a6b2b6082f459
Ola Ali <ola.ali@ibtikar.net.sa>
2017-01-02 14:16:29 +0300

    Edit Magazine section page make page unmodified

--- DataFixtures/MongoDB/LoadPageData.php
+++ DataFixtures/MongoDB/LoadPageData.php
@@ -24,6 +24,7 @@ class LoadPageData implements FixtureInterface
 Our Magazines are rich with ideas, recipes and entertaining innovative solutions that enrich your life and the lives of your family, you will find it in each issue of the Goody kitchen Magazine.
 Join more than half a million females that have read the magazine and benefited from the content of the printed copies and through the application of Goody Kitchen Magazine on App store and Google play.');
         $page->setUrl('https://land.ly/goody');
+        $page->setNotModified(TRUE);
         $manager->persist($page);
         $manager->flush();
     }
