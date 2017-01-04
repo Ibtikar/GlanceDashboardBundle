d44477732c2f096a395f4de95e1d55fc29ea6dc8
Ola Ali <ola.ali@ibtikar.net.sa>
2017-01-04 13:03:45 +0300

    change dot from predifed data in magazine page

--- DataFixtures/MongoDB/LoadPageData.php
+++ DataFixtures/MongoDB/LoadPageData.php
@@ -18,11 +18,11 @@ class LoadPageData implements FixtureInterface
         $page->setTitle('magazine page');
         $page->setBrief('مرجع متكامل مليء بالوصفات والأفكار والإلهام المتجدد!
 واحة غنية بالأفكار والوصفات والحلول المبتكرة والممتعة التي تثري حياتك وحياة عائلتك، تجدينها في كل عدد من أعداد مجلة مطبخ قودي.
-انضمي إلى أكثر من نصف مليون سيدة قرأت المجلة واستفادت من محتواها بنسختها الورقية، وعبر تطبيق مجلة مطبخ قودي على الآندرويد ومتجر التطبيقات.');
+انضمي إلى أكثر من نصف مليون سيدة قرأت المجلة واستفادت من محتواها بنسختها الورقية، وعبر تطبيق مجلة مطبخ قودي على الآندرويد ومتجر التطبيقات');
         $page->setBriefEn('A complete reference of recipes, ideas and inspirations!
 
 Our Magazines are rich with ideas, recipes and entertaining innovative solutions that enrich your life and the lives of your family, you will find it in each issue of the Goody kitchen Magazine.
-Join more than half a million females that have read the magazine and benefited from the content of the printed copies and through the application of Goody Kitchen Magazine on App store and Google play.');
+Join more than half a million females that have read the magazine and benefited from the content of the printed copies and through the application of Goody Kitchen Magazine on App store and Google play');
         $page->setUrl('https://land.ly/goody');
         $page->setNotModified(TRUE);
         $manager->persist($page);
