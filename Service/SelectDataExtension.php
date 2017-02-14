c605ce7e030f9828f178fcaa0f2d3fefaa6225bf
gehad mohamed <gehad.mohamed.dev@gmail.com>
2017-02-14 13:31:32 +0200

    search in recipes

---
+++ Service/SelectDataExtension.php
@@ -0,0 +1,32 @@
+<?php
+namespace Ibtikar\GlanceDashboardBundle\Service;
+
+
+class propertyAccessorExtension extends \Twig_Extension
+{
+
+    private $translator;
+
+    public function __construct($translator) {
+        $this->translator = $translator;
+
+       }
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction(
+                'property_access', array($this, 'propertyAccess')
+            )
+        );
+    }
+
+    public function propertyAccess($object,$functionName,$fieldAccess)
+    {
+       $functionToBeCalled='get'.ucfirst($functionName);
+       $functionToAccessField='get'.ucfirst($fieldAccess);
+       $calledObject=$object->$functionToBeCalled();
+       return $object->$functionToBeCalled()?method_exists($object->$functionToBeCalled(),$functionToAccessField)?$object->$functionToBeCalled()->$functionToAccessField():$object->$functionToBeCalled()->__toString() :'';
+    }
+
+}
