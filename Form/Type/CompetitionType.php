7c650389c91f8c08d2f9360bdf6a7feeb5054522
Gehad <gehad.mohamed@ibtikar.net.sa>
2017-03-06 17:12:46 +0200

    competition not good

--- Form/Type/CompetitionType.php
+++ Form/Type/CompetitionType.php
@@ -23,7 +23,13 @@ class CompetitionType extends AbstractType {
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
+
         $transformer = new DateTimeTransformer();
+
+        $date = new \DateTime();
+
+        $minDate = $date->modify('+1 hour')->format(\DateTime::ISO8601);
+
         $builder
                 ->add('title', formType\TextType::class, array('attr' => array('data-rule-minlength' => 3,'data-rule-maxlength' => 150)))
                 ->add('titleEn', formType\TextType::class, array('attr' => array('data-rule-minlength' => 3,'data-rule-maxlength' => 150)))
@@ -34,9 +40,10 @@ class CompetitionType extends AbstractType {
 
         if($this->isNew){
             $builder->add('questions', formType\CollectionType::class, array('label' => false,'entry_type' => QuestionType::class, 'allow_add' => true, 'allow_delete' => true, 'by_reference' => true, 'attr' => array('class' => 'competitionQuestion')));
+            $builder->add('questionsEn', formType\CollectionType::class, array('label' => false,'entry_type' => QuestionType::class, 'allow_add' => true, 'allow_delete' => true, 'by_reference' => true, 'attr' => array('class' => 'competitionQuestion')));
         }
 
-        $builder->add($builder->create('expiryDate', formType\DateType::class, array('required' => false,'widget' => 'single_text', 'attr' => array('data-date-start-date' => isset($this->extraOptions['data-date-start-date'])?$this->extraOptions['data-date-start-date']:'+1d', 'data-rule-dateAfterToday' => '')))->addViewTransformer($transformer));
+        $builder->add($builder->create('expiryDate', formType\DateType::class, array('required' => false,'widget' => 'single_text', 'attr' => array('data-min-date'=>$minDate,'data-date-start-date' => isset($this->extraOptions['data-date-start-date'])?$this->extraOptions['data-date-start-date']:'+1d', 'data-rule-dateAfterToday' => '')))->addViewTransformer($transformer));
 
         if($this->isNew){
             $builder->add('allowedToVote', formType\ChoiceType::class, array('choices' => Competition::$allowedVoters, 'expanded' => true, 'attr'  => array('data-error-after-selector' => '#competition_type_allowedToVote')));
