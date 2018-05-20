45a4728d5807dbe0cf0e50c4a58d5e5267b2a150
ola-ali1 <ola.ali@ibtikar.net.sa>
2018-05-20 17:10:16 +0200

    modifications in 404 page

--- Service/NotFoundExceptionListener.php
+++ Service/NotFoundExceptionListener.php
@@ -12,11 +12,12 @@ class NotFoundExceptionListener
 
     private $templating;
     private $router;
+    private $translator;
 
-    public function __construct($templating,$router)
+    public function __construct($templating,$router,$translator)
     {
         $this->templating = $templating;
-        $this->router = $router;
+        $this->translator = $translator;
 
     }
 
@@ -30,10 +31,30 @@ class NotFoundExceptionListener
                 $response = $this->templating->renderResponse('IbtikarGlanceDashboardBundle:Exception:error.html.twig');
                 $event->setResponse($response);
             } else {
+                 $locale='ar';
+                 $newurl=$event->getRequest()->getRequestUri();
+
+                if (strpos($event->getRequest()->getRequestUri(), '/en/')!==false) {
+                    $locale='en';
+                    $newurl= str_replace('/en/','/ar/', $event->getRequest()->getRequestUri());
+
+
+                }else{
+                    $locale='ar';
+                    $newurl= str_replace('/ar/','/en/', $event->getRequest()->getRequestUri());
+
+
+                }
+
+
 //                \Doctrine\Common\Util\Debug::dump($this->context->getToken());
 //                exit;
 //                $event->setResponse(new RedirectResponse($this->router->generate('ibtikar_goody_frontend_error_404')));
-                $response = $this->templating->renderResponse('IbtikarGoodyFrontendBundle:Exception:error.html.twig');
+                $this->translator->setLocale($locale);
+                $event->getRequest()->setLocale($locale);
+                $event->getRequest()->attributes->set('_locale', $locale);
+
+                $response = $this->templating->renderResponse('IbtikarGoodyFrontendBundle:Exception:error.html.twig',array('locale'=>$locale,'url'=>$newurl));
                 $event->setResponse($response);
 
             }
